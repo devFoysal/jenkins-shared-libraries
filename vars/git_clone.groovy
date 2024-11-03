32a7b20e3ad8f036ca8a984473da5570c8b27e78
Foysal Mahmud <49339416+devFoysal@users.noreply.github.com>
2024-11-03 14:24:49 +0600

    Update git_clone.groovy

--- vars/git_clone.groovy
+++ vars/git_clone.groovy
@@ -1,4 +1,4 @@
-dev call(String GitUrl, String GitBranch){
+def call(String GitUrl, String GitBranch){
   echo "This is cloning the code"
   git url: "${GitUrl}", branch:"${GitBranch}"
   echo "Code cloning successful"Ï
